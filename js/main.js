b6b11bdff46885ac12ab6bfff2385c3af2fcd3dd
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-08 17:48:09 +0000

    update counter

--- js/main.js
+++ js/main.js
@@ -4,44 +4,62 @@ const inputUserNumber = document.querySelector(".js_inputUserNumber");
 const btnTry = document.querySelector(".js_btnTry");
 const textTry = document.querySelector(".js_textTry");
 const attemps = document.querySelector(".js_attempts");
+const btnReset = document.querySelector(".js_btnReset");
+let movesComputer = getRandomNumber();
 
-let clicksPlayer = 0;
+let clicksPlayer = "";
 
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
-}
 function updateScore() {
+  clicksPlayer++;
   attemps.innerHTML = `Número de intentos: ${clicksPlayer}`;
 }
 
 function getUserNumber() {
-  event.preventDefault();
   return parseInt(inputUserNumber.value);
 }
 
-function play() {
-  const movesPlayer = getUserNumber();
-  const movesComputer = getRandomNumber(100);
-  clicksPlayer++;
+function showText(message) {
+  textTry.innerHTML = message;
+}
+function play(event) {
+  event.preventDefault();
 
-  if (isNaN(movesPlayer) || movesPlayer < 1 || movesPlayer > 100) {
-    textTry.innerHTML = "El número debe estar entre 1 y 100";
-  } else {
-    if (movesPlayer > movesComputer) {
-      textTry.innerHTML = "Demasiado alto";
-    } else if (movesPlayer < movesComputer) {
-      textTry.innerHTML = "Demasiado bajo";
-    } else if (movesPlayer === movesComputer) {
-      textTry.innerHTML = "¡Has ganado, campeona!";
-      // Recargar la página después de un breve retraso (por ejemplo, 2 segundos)
-      setTimeout(function () {
-        window.location.reload();
-      }, 2000);
-      return; // Terminar la función para evitar ejecutar el resto del código
+  if (clicksPlayer < 10) {
+    const movesPlayer = getUserNumber();
+
+    if (isNaN(movesPlayer) || movesPlayer < 1 || movesPlayer > 100) {
+      showText("El número debe estar entre 1 y 100");
+    } else {
+      if (movesPlayer > movesComputer) {
+        showText("Demasiado alto");
+      } else if (movesPlayer < movesComputer) {
+        showText("Demasiado bajo");
+      } else if (movesPlayer === movesComputer) {
+        showText("¡Has ganado, campeona!");
+      }
+      console.log(movesComputer);
     }
+  } else {
+    showText(
+      "El juego ha terminado, has alcanzado el número de intentos máximo."
+    );
   }
+
   updateScore();
-  console.log(movesComputer);
+}
+
+function resetGame(event) {
+  event.preventDefault();
+  clicksPlayer = 0;
+  movesComputer = getRandomNumber();
+  attemps.innerHTML = "Número de intentos: 0";
+  textTry.innerHTML = "";
+  inputUserNumber.value = "";
 }
 
 btnTry.addEventListener("click", play);
+btnReset.addEventListener("click", resetGame);
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
